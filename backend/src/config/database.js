c5e2e7e3da37469f0eee5c206618cb58c28f958f
leoslemos <leoslemos@users.noreply.github.com>
2019-12-14 18:15:52 -0300

    Alteração Conexão BD
    Alterado para banco de dados mongoDB

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,17 +1,19 @@
-const mysql = require('mysql')
-class ConexaoBD {
-  static connect(){
-    const conn = mysql.createConnection({
-    host:     'localhost',
-    user:     'root',
-    password: 'root',
-    database: 'db_todoapp'
-  })
-  conn.connect();
-  return conn;
-}
+// conexão mongo
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise //para tirar a advertencia da API Promise, por estar desatualizada
+module.exports = mongoose.connect('mongodb://localhost/todo')
 
-// // conexão mongo
-// const mongoose = require('mongoose')
-// mongoose.Promise = global.Promise //para tirar a advertencia da API Promise, por estar desatualizada
-// module.exports = mongoose.connect('mongodb://localhost/todo')
+
+// const mysql = require('mysql')
+// class ConexaoBD {
+//   static connect(){
+//     const conn = mysql.createConnection({
+//     host:     'localhost',
+//     user:     'root',
+//     password: 'root',
+//     database: 'db_todoapp'
+//   })
+//   conn.connect();
+//   return conn;
+// }
+
